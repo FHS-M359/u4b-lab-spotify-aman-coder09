2a0ebf949f32267aedb5d356f97b98fd40de8dc6
aman-coder09 <meet.aaman.s@gmail.com>
2026-02-17 21:47:09 -0600

    Day2
    done reading files . will be working on linear search

--- U4BLab/src/Playlist.java
+++ U4BLab/src/Playlist.java
@@ -1,4 +1,52 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Playlist {
+
+    private ArrayList<Song> songs;
+
+    public Playlist(){
+        songs = new ArrayList<Song>();
+    }
+
+    public void readingSongs(String filename) throws Exception{
+
+        Scanner inF = new Scanner(new File(filename));
+
+        while (inF.hasNextLine()){
+
+            String line = inF.nextLine();
+
+            String[] dividing = line.split(",");
+
+            String title = dividing[0];
+            String artist = dividing[1];
+            String album = dividing[2];
+            int durationSeconds = Integer.parseInt(dividing[3]);
+            int year = Integer.parseInt(dividing[4]);
+            String genre = dividing[5];
+
+            Song s = new Song(artist,title,album,durationSeconds,year,genre);
+
+            songs.add(s);
+        }
+        inF.close();
+
+
+
+
+
+    }
+
+    public String toString(){
+
+        String answer = "";
+       for (Song s : songs){
+           answer= answer+ s + "\n";
+       }
+        return answer;
+    }
 
 
 }
--- U4BLab/src/SpotifyTester.java
+++ U4BLab/src/SpotifyTester.java
@@ -1,4 +1,10 @@
 public class SpotifyTester {
 
     //HI
+    public static void main(String[] args) throws Exception {
+        Playlist playlist1 = new Playlist();
+        playlist1.readingSongs("C:\\APCSA\\M359-WORKREPO\\u4b-lab-spotify-aman-coder09\\U4BLab\\spotify_unique_years_artists.txt");
+
+        System.out.println(playlist1);
+    }
 }
